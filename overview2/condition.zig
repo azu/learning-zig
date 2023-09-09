473dc8ff927109f1cc822922aaff871343a0c755
azu <azuciao@gmail.com>
2023-09-10 07:40:23 +0900

    switchはelseを使って網羅しないとエラーになる

--- overview2/condition.zig
+++ overview2/condition.zig
@@ -5,4 +5,20 @@ pub fn main() void {
     } else {
         std.log.info("not equal\n", .{});
     }
+
+    const retAnn = anniversaryName(1);
+    _ = retAnn;
+}
+
+fn anniversaryName(years_married: u16) []const u8 {
+    switch (years_married) {
+        1 => return "paper",
+        2 => return "cotton",
+        3 => return "leather",
+        4 => return "flower",
+        5 => return "wood",
+        6 => return "sugar",
+        // defaultcase
+        else => return "default",
+    }
 }
