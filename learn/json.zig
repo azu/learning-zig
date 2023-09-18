dea43ffbd2364cc6bb256053d2f6b22b3306efb4
azu <azuciao@gmail.com>
2023-09-18 09:23:35 +0900

    ignore_unknown_fieldsを渡すと未知のフィールドがあっても問題なくなる

--- learn/json.zig
+++ learn/json.zig
@@ -5,14 +5,9 @@ const eql = std.mem.eql;
 
 test "json parse with strings" {
     const User = struct { name: []u8, age: u16 };
-
-    const parsed = try std.json.parseFromSlice(
-        User,
-        test_allocator,
-        \\{ "name": "Joe", "age": 25 }
-    ,
-        .{},
-    );
+    const parsed = try std.json.parseFromSlice(User, test_allocator,
+        \\{ "name": "Joe", "age": 25, "t": 1 }
+    , .{ .ignore_unknown_fields = true });
     defer parsed.deinit();
 
     const user = parsed.value;
